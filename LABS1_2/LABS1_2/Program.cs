a5bca39f4b55bfacfd9bb686974c802f87c4fae3
IAnthropos <beeornotbee@yandex.ru>
2023-04-25 03:08:55 +0300

    Lab1_2 solution

--- LABS1_2/LABS1_2/Program.cs
+++ LABS1_2/LABS1_2/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace LABS1_2
+{
+    /*
+     * Вариант № 4
+     */
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var triangle = new Triangle(10, 8, 0.89605203797);
+            Console.WriteLine($"a = {triangle.La}, b = {triangle.Lb}, c = {triangle.Lc}");
+            Console.WriteLine($"<a = {triangle.AngleA}, <b = {triangle.AngleB}, <c = {triangle.AngleC}");
+            Console.WriteLine($"Check sum of angles: {triangle.AngleA + triangle.AngleB + triangle.AngleC}");
+            Console.WriteLine($"S = {triangle.CalculateS()}");
+        }
+    }
+}
---
+++ LABS1_2/LABS1_2/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LABS1_2
+{
+    public class Triangle
+    {
+        public double La { get; set; }
+        public double Lb { get; set; }
+        public double Lc { get; set; }
+        public double AngleC { get; set; }
+        public double AngleB { get; set; }
+        public double AngleA { get; set; }
+        public Triangle(double la, double lb, double angleC)
+        {
+            La = la;
+            Lb = lb;
+            AngleC = angleC;
+            Lc = Math.Sqrt(Math.Pow(La, 2) + Math.Pow(Lb, 2) - 2 * La * Lb * Math.Cos(AngleC));
+            AngleA = Math.Asin((Math.Sin(angleC) * La) / Lc);
+            AngleB = Math.Asin((Math.Sin(angleC) * Lb) / Lc);
+        }
+
+        public double CalculateS()
+        {
+            return La * Lb * Math.Sin(AngleC) / 2;
+        }
+    }
+}
+
